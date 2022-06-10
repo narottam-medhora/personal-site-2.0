1b5a8851acbb071b7b971ab751aff05587df8d6f
Narottam Medhora <narottammedhora@gmail.com>
2022-06-10 18:58:17 +0100

    Creates landing page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,49 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<html lang="en">
+    <body>
+        <p class="line-1 anim-typewriter">Under (re)construction. Come back soon!</p>
+    </body>
+</html>
+
+<style>
+    /* Global */
+    html{
+        min-height: 100%;
+        overflow: hidden;
+    }
+
+    body{
+        height: calc(100vh - 8em);
+        padding: 4em;
+        color: rgba(255,255,255,.75);
+        font-family: 'Courier New', Courier, monospace;  
+        background-color: rgb(25,25,25);  
+    }
+
+    .line-1 {
+        position: relative;
+        top: 50%;  
+        width: 24em;
+        margin: 0 auto;
+        border-right: 2px solid rgba(255,255,255,.75);
+        font-size: 180%;
+        text-align: center;
+        white-space: nowrap;
+        overflow: hidden;
+        transform: translateY(-50%);    
+    }
+
+    /* Animation */
+    .anim-typewriter{
+        animation: typewriter 4s steps(44) 1s 1 normal both, blinkTextCursor 500ms steps(44) infinite normal;
+    }
+
+    @keyframes typewriter{
+    from{width: 0;}
+    to{width: 24em;}
+    }
+
+    @keyframes blinkTextCursor{
+    from{border-right-color: rgba(255,255,255,.75);}
+    to{border-right-color: transparent;}
+    }
+</style>
