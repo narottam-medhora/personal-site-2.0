386c80af0b0b72df52bc3292d347e11a627c45c8
Narottam Medhora <narottammedhora@gmail.com>
2022-06-27 14:32:17 +0100

    Fixes stylesheet imports

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,9 @@
-<!-- Import components -->
 <script>
+    // Import styles
+    import '../reset.css';
+    import '../global.css'
+
+    // Import components
     import Placeholder from "../components/Placeholder.svelte";
     import Footer from "../components/Footer.svelte";
 </script>
